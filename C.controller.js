75498aa6d3751261c1cdc21458faedbcffb8dadf
gnseo <gnseo@bsgglobal.com>
2023-06-05 10:25:35 +0900

    use CheckBox for example

--- C.controller.js
+++ C.controller.js
@@ -21,65 +21,19 @@ sap.ui.define(
           .setData({
             items: [
               {
-                myValue: "",
-              },
-              {
-                myValue: "",
-              },
-            ],
-            suggestions: [
-              {
+                key: "ekansh2",
                 name: "ekansh",
               },
               {
+                key: "saxena2",
                 name: "saxena",
               },
-            ],
+            ]
           });
       },
 
       onChange: function (oEvent) {
-        console.log("Change event called");
-      },
-
-      onValueHelpRequest: function (oEvent) {
-        var sInputValue = oEvent.getSource().getValue(),
-          oView = this.getView();
-
-        if (!this._pValueHelpDialog) {
-          this._pValueHelpDialog = Fragment.load({
-            id: oView.getId(),
-            name: "sap.m.sample.InputAssisted.ValueHelpDialog",
-            controller: this,
-          }).then(function (oDialog) {
-            oView.addDependent(oDialog);
-            return oDialog;
-          });
-        }
-        this._pValueHelpDialog.then(function (oDialog) {
-          // Create a filter for the binding
-          oDialog.getBinding("items").filter([new Filter("Name", FilterOperator.Contains, sInputValue)]);
-          // Open ValueHelpDialog filtered by the input's value
-          oDialog.open(sInputValue);
-        });
-      },
-
-      onValueHelpSearch: function (oEvent) {
-        var sValue = oEvent.getParameter("value");
-        var oFilter = new Filter("Name", FilterOperator.Contains, sValue);
-
-        oEvent.getSource().getBinding("items").filter([oFilter]);
-      },
-
-      onValueHelpClose: function (oEvent) {
-        var oSelectedItem = oEvent.getParameter("selectedItem");
-        oEvent.getSource().getBinding("items").filter([]);
-
-        if (!oSelectedItem) {
-          return;
-        }
-
-        this.byId("productInput").setValue(oSelectedItem.getTitle());
+        console.warn("Change event called", oEvent.getParameters());
       },
     });
   }
